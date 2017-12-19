d1e29bc931d1945b251392c3b5b1d39b89881473
Irfan Vigma Taufik <davigmacode@gmail.com>
2017-12-19 18:02:37 +0700

    platforms search result only return the most relevan based on keyword for the first 25 item

--- src/facebook/platforms.js
+++ src/facebook/platforms.js
@@ -9,8 +9,8 @@ class Platforms {
 
   search (options) {
     return new Promise((resolve, reject) => {
-      // define request
-      let request = {
+      // request to the graph api
+      this.api.request({
         method: 'get',
         url: 'search',
         params: {
@@ -20,24 +20,7 @@ class Platforms {
           'fields': 'id,name,username,fan_count,link,picture',
           'limit': 25
         }
-      };
-
-      // if prev page exist
-      if (options.before) {
-        request.params.before = options.before;
-      }
-
-      // if next page exist
-      if (options.after) {
-        request.params.after = options.after;
-      }
-
-      options.platforms = options.platforms || [];
-      options.counter = options.counter || 0;
-      options.counter = options.counter + 1;
-
-      // request first page
-      this.api.request(request).then((results) => {
+      }).then((results) => {
         let response = results.data;
         let platforms = response.data.map((item) => {
           item.fans = item.fan_count;
@@ -45,18 +28,7 @@ class Platforms {
           delete item.fan_count;
           return item;
         });
-        options.platforms = options.platforms.concat(platforms);
-        options.platforms = options.platforms.sort((a, b) => a['fans'] < b['fans'] ? 1 : -1);
-
-        if (response.paging && options.counter < 2) {
-          // set paging cursor
-          options.after = response.paging.cursors.after;
-
-          // call another request
-          resolve(this.search(options));
-        } else {
-          resolve(options.platforms);
-        }
+        resolve(platforms);
       }).catch((http) => {
         reject(http.response.data.error);
       });
